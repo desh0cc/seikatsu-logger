ff917e12a01b94adfe69e4229f71650f12fffc04
Kirill Konakh <desh0ccfuture@gmail.com>
2024-12-24 19:52:18 +0200

    Update settings_page.py

--- libs/pages/settings_page.py
+++ libs/pages/settings_page.py
@@ -61,6 +61,14 @@ def settings_page(page: ft.Page) -> ft.View:
         model = e.control.value
         save_cfg(model=model)
 
+    def on_hover(e):
+        e.control.content = (
+            ft.Image(src="assets/icons/open-file.svg", width=24,height=24,color=get_time_based_color()) if
+            e.data == "true" 
+            else ft.Icon(ft.Icons.FOLDER_ROUNDED,color=get_time_based_color())
+        )
+        page.update()
+
     file_picker = ft.FilePicker(on_result=save_cfg)
     page.overlay.append(file_picker)
 
@@ -71,7 +79,6 @@ def settings_page(page: ft.Page) -> ft.View:
         controls=[
             navigator.add(),
             
-            ft.Container(ft.Text("About App", size=20,weight=ft.FontWeight.W_600),alignment=ft.alignment.center),
 
             ft.Container(
                 ft.Column([
@@ -79,10 +86,21 @@ def settings_page(page: ft.Page) -> ft.View:
                         ft.Image(src="icon.png",width=72,height=72),
                         alignment=ft.alignment.center
                     ),
+
+                    ft.Container(
+                        ft.Text(
+                                lang_load("settings_page_about_title"), 
+                                size=20,
+                                font_family="CaskaydiaCove",
+                                weight=ft.FontWeight.W_600
+                            ),
+                        alignment=ft.alignment.center
+                    ),
+
                     ft.Row([
                         ft.Container(
                             ft.Row([
-                                ft.IconButton(content=ft.Image(src="icons/github.png"),url="https://github.com/desh0cc",width=32,height=32),
+                                ft.IconButton(content=ft.Image(src="icons/github.png"),url="https://github.com/desh0cc/seikatsu-logger",width=32,height=32),
                                 ft.IconButton(content=ft.Image(src="icons/telegram.png"),url="https://t.me/+9Olu761hIZ4zYTJi",width=32,height=32)
                             ], alignment=ft.MainAxisAlignment.CENTER),
                             alignment=ft.alignment.center,
@@ -93,13 +111,13 @@ def settings_page(page: ft.Page) -> ft.View:
                     ], alignment=ft.MainAxisAlignment.CENTER)
                 ]),
             ),
-
+            
             ft.Container(
                 ft.Column([
                     ft.Container(
                         ft.Row([
                             ft.Container(
-                                ft.Text("Version:"),
+                                ft.Text(lang_load("settings_page_version")),
                                 expand=1,
                                 alignment=ft.alignment.center_left,
                                 padding=ft.padding.only(left=10, top=5)
@@ -115,7 +133,7 @@ def settings_page(page: ft.Page) -> ft.View:
                     ft.Container(
                         ft.Row([
                             ft.Container(
-                                ft.Text("Developer:"),
+                                ft.Text(lang_load("settings_page_developer")),
                                 expand=1,
                                 alignment=ft.alignment.center_left,
                                 padding=ft.padding.only(left=10)
@@ -130,18 +148,18 @@ def settings_page(page: ft.Page) -> ft.View:
                         padding=ft.padding.only(top=-5,bottom=7)
                     )
                 ]),
-                bgcolor=ft.colors.with_opacity(0.4, color="#333333"),
+                bgcolor=ft.Colors.with_opacity(0.4, color="#333333"),
                 border_radius=15,
             ),
 
-            ft.Container(ft.Text("Configuration", size=20,weight=ft.FontWeight.W_600),alignment=ft.alignment.center,padding=ft.padding.only(top=7)),
+            ft.Container(ft.Text(lang_load("settings_page_configuration"), size=20,font_family="CaskaydiaCove",weight=ft.FontWeight.W_600),alignment=ft.alignment.center,padding=ft.padding.only(top=7)),
 
             ft.Container(
                 ft.Column([
                     ft.Container(
                         ft.Row([
                             ft.Container(
-                                ft.Text("Language"),
+                                ft.Text(lang_load("settings_page_language")),
                                 padding=ft.padding.only(left=10,top=5)
                             ),
                             ft.Container(
@@ -154,9 +172,9 @@ def settings_page(page: ft.Page) -> ft.View:
                                     width=90,
                                     bgcolor=None,
                                     border=None,
-                                    border_color=ft.colors.with_opacity(0.0, "#FFFFFF"),
-                                    fill_color=ft.colors.with_opacity(0.0, "#FFFFFF"),
-                                    focused_bgcolor=ft.colors.with_opacity(0.0, "#FFFFFF"),
+                                    border_color=ft.Colors.with_opacity(0.0, "#FFFFFF"),
+                                    fill_color=ft.Colors.with_opacity(0.0, "#FFFFFF"),
+                                    focused_bgcolor=ft.Colors.with_opacity(0.0, "#FFFFFF"),
                                     content_padding=ft.padding.symmetric(horizontal=0),
                                     alignment=ft.alignment.center,
                                 ),
@@ -169,7 +187,7 @@ def settings_page(page: ft.Page) -> ft.View:
                     ft.Container(
                         ft.Row([
                             ft.Container(
-                                ft.Text("Model"),
+                                ft.Text(lang_load("settings_page_model")),
                                 padding=ft.padding.only(left=10,top=5)
                             ),
                             ft.Container(
@@ -182,32 +200,44 @@ def settings_page(page: ft.Page) -> ft.View:
                                     width=90,
                                     bgcolor=None,
                                     border=None,
-                                    border_color=ft.colors.with_opacity(0.0, "#FFFFFF"),
-                                    fill_color=ft.colors.with_opacity(0.0, "#FFFFFF"),
-                                    focused_bgcolor=ft.colors.with_opacity(0.0, "#FFFFFF"),
+                                    border_color=ft.Colors.with_opacity(0.0, "#FFFFFF"),
+                                    fill_color=ft.Colors.with_opacity(0.0, "#FFFFFF"),
+                                    focused_bgcolor=ft.Colors.with_opacity(0.0, "#FFFFFF"),
                                     content_padding=ft.padding.symmetric(horizontal=0),
                                     alignment=ft.alignment.center,
                                 ),
-                                expand=1,
-                                alignment=ft.alignment.center_right,
                                 padding=ft.padding.only(top=5,right=10)
                             )
-                        ]),
+                        ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
                         padding=ft.padding.only(top=-15)
                     ),
                     ft.Container(
                         content=ft.Row([
                             ft.Container(
-                                ft.Text("Note path"),
+                                ft.Text(lang_load("settings_page_note_path")),
                                 alignment=ft.alignment.center_left,
                                 padding=ft.padding.only(left=10)
                             ),
                             ft.Container(
-                                ft.IconButton(
-                                    icon=ft.icons.FOLDER,
-                                    icon_color=get_time_based_color(),
-                                    on_click=lambda _: file_picker.get_directory_path(lang_load("settings_page_folder_dialog"))
-                                ),
+                                ft.Row([
+                                    ft.Container(
+                                        ft.Text(
+                                            config.get("folder_path"),
+                                            color=ft.Colors.BLACK,
+                                            max_lines=1,
+                                            overflow=ft.TextOverflow.ELLIPSIS
+                                        ),
+                                        width=200,
+                                        bgcolor=ft.Colors.with_opacity(0.85, "#FFFFFF"),
+                                        border_radius=15,
+                                        padding=ft.padding.all(5)
+                                    ),
+                                    ft.ElevatedButton(
+                                        content=ft.Icon(ft.Icons.FOLDER_ROUNDED, color=get_time_based_color()),
+                                        on_hover=on_hover,
+                                        on_click=lambda _: file_picker.get_directory_path(lang_load("settings_page_folder_dialog"))
+                                    ),
+                                ], alignment=ft.MainAxisAlignment.END), 
                                 expand=1,
                                 alignment=ft.alignment.center_right,
                                 padding=ft.padding.only(right=10)
@@ -216,7 +246,7 @@ def settings_page(page: ft.Page) -> ft.View:
                         padding=ft.padding.only(top=-15)
                     )
                 ]),
-                bgcolor=ft.colors.with_opacity(0.4, "#333333"),
+                bgcolor=ft.Colors.with_opacity(0.4, "#333333"),
                 border_radius=15
             ),
 
